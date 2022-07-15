ca9550e077e33e9f98f8c8cd2ca80b9e02b85913
ggarber42 <ggarber@protonmail.com>
2022-07-14 21:36:18 -0300

    feat: app drawer

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../screens/config_screen.dart';
+import '../screens/list_models_screen.dart';
+
+class MainDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: Text('Opções'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.ballot_rounded),
+            title: Text('Modelos'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(ListModelsScreen.routeName);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.filter_vintage_sharp),
+            title: Text('Configurações'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(ConfigScreen.routeName);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
