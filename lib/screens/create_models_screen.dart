975cb0d2c6d2e3ae9c1095c88f4e54cafe98e98c
ggarber42 <ggarber@protonmail.com>
2022-07-14 22:52:22 -0300

    feat(ui): add screen elements

--- lib/screens/create_models_screen.dart
+++ lib/screens/create_models_screen.dart
@@ -1,18 +1,45 @@
+import 'package:collect_app/screens/add_models_widgets_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/main_bar.dart';
 import '../widgets/main_drawer.dart';
 
 class CreateModelsScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    double appHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
       appBar: MainBar(),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('Criando Modelos de Formulários')],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
+                  hintText: 'Pesquise por modelos prontos',
+                ),
+              ),
+            ),
+            SizedBox(height: appHeight / 3),
+            Container(
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => AddModelsWidgetScreen()),
+                    );
+                  },
+                  child: Text('Criar Modelo'),
+                ),
+              ),
+            )
+          ],
         ),
       ),
       drawer: MainDrawer(),
