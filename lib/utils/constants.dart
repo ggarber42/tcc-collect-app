d93e9e2d3850a901a08110cec7f8494e23c05fc0
ggarber42 <ggarber@protonmail.com>
2022-07-21 22:12:18 -0300

    feat: constant list

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const DropMenuOptions = const [
+  DropdownMenuItem(child: Text('Grupo radio'), value: 'radio'),
+  DropdownMenuItem(child: Text('Texto'), value: 'text'),
+];
