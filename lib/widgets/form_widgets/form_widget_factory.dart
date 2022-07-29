f7ac767a5370436a36695d18dee4bb9db60d0532
ggarber42 <ggarber@protonmail.com>
2022-07-29 00:39:15 -0300

    fix: add context to method argument

--- lib/widgets/form_widgets/form_widget_factory.dart
+++ lib/widgets/form_widgets/form_widget_factory.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/material.dart';
+
 import 'form_widget_interface.dart';
 import 'form_widget_radio.dart';
 
 class FormWidgetFactory {
-  FormWidget createFormField(String name){
+  FormWidget createFormField(BuildContext context, String name){
     if(name == 'radio'){
       FormWidgetRadio radio = new FormWidgetRadio();
-      radio.showCreateDialog();
+      radio.showCreateDialog(context);
       return radio;
     }
     return new FormWidgetRadio();
