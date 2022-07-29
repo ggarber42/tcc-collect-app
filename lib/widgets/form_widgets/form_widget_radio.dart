bf46946678751eba55c90f6d411e39ba5aeb223d
ggarber42 <ggarber@protonmail.com>
2022-07-29 00:40:02 -0300

    feat: add createDialog

--- lib/widgets/form_widgets/form_widget_radio.dart
+++ lib/widgets/form_widgets/form_widget_radio.dart
@@ -2,32 +2,44 @@ import 'package:flutter/material.dart';
 
 import 'form_widget_interface.dart';
 
-class FormWidgetRadio extends FormWidget{
+class FormWidgetRadio extends FormWidget {
   String? name;
 
   FormWidgetRadio();
 
   FormWidgetRadio.dialog(this.name);
 
-
   @override
   Widget getWidgetBody() {
     return ListTile(
-          title: Text('Misha'),
-          leading: Radio(
-            value: 1,
-            groupValue: 1,
-            onChanged: (value){
-              print(value);
-            }
-          ),
-        );
+      title: Text('Misha'),
+      leading: Radio(
+          value: 1,
+          groupValue: 1,
+          onChanged: (value) {
+            print(value);
+          }),
+    );
   }
 
   @override
-  void showCreateDialog() {
-    // TODO: implement showCreateDialog
+  Future<dynamic> showCreateDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            title: Text('Wanna Exit?'),
+            actions: [
+              FlatButton(
+                onPressed: () => Navigator.pop(context, false), // passing false
+                child: Text('No'),
+              ),
+              FlatButton(
+                onPressed: () => Navigator.pop(context, true), // passing true
+                child: Text('Yes'),
+              ),
+            ],
+          );
+        });
   }
- 
-
 }
