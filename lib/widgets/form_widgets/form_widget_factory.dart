140af8a9d4a30d342d003871bd9263b985dfb412
ggarber42 <ggarber@protonmail.com>
2022-08-02 23:46:51 -0300

    feat: add text widget validatio

--- lib/widgets/form_widgets/form_widget_factory.dart
+++ lib/widgets/form_widgets/form_widget_factory.dart
@@ -1,14 +1,18 @@
+import 'package:collect_app/widgets/form_widgets/form_widget_text.dart';
 import 'package:flutter/material.dart';
 
 import 'form_widget_interface.dart';
 import 'form_widget_radio.dart';
 
 class FormWidgetFactory {
-  FormWidget createFormField(BuildContext context, String name){
-    if(name == 'radio'){
+  FormWidget createFormField(BuildContext context, Map<dynamic, dynamic> res){
+    if(res['selectedValue'] == 'radio'){
       FormWidgetRadio radio = new FormWidgetRadio();
       radio.showCreateDialog(context);
       return radio;
+    } else if(res['selectedValue'] == 'text'){
+      FormWidgetText text = new FormWidgetText(res['name']);
+      return text;
     }
     return new FormWidgetRadio();
   }
