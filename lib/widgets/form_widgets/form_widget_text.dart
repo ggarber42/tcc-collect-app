371044d8174dd40aa5f7b493094d17f3fd77c03a
ggarber42 <ggarber@protonmail.com>
2022-08-08 21:46:44 -0300

    feat: form validation text widget

--- lib/widgets/form_widgets/form_widget_text.dart
+++ lib/widgets/form_widgets/form_widget_text.dart
@@ -3,19 +3,63 @@ import 'package:flutter/material.dart';
 import 'form_widget_interface.dart';
 
 class FormWidgetText implements FormWidget {
-  String name;
+  String? _name;
 
-  FormWidgetText(this.name);
+  final TextEditingController _textEditingController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  set name(String value) {
+    _name = value;
+  }
 
   @override
   Widget getWidgetBody() {
     return TextFormField(
-        decoration: InputDecoration(
-          labelText: '$name',
-        ),
-        onSaved: (String? value) {
-          print(value);
-        });
+      decoration: InputDecoration(
+        labelText: '$_name',
+      ),
+      onSaved: (_) {},
+    );
   }
 
+  @override
+  Future showCreateDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            title: Text('Nome do campo'),
+            content: Form(
+              key: _formKey,
+              child: TextFormField(
+                controller: _textEditingController,
+                decoration: InputDecoration(labelText: 'Digite aqui'),
+                textInputAction: TextInputAction.done,
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            actions: [
+              FlatButton(
+                onPressed: () => Navigator.pop(context, false),
+                child: Text('Cancel'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _name = _textEditingController.value.text;
+                    return Navigator.pop(context, true);
+                  }
+                  return;
+                },
+                child: Text('Ok'),
+              ),
+            ],
+          );
+        });
+  }
 }
