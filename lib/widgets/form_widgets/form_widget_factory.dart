841516ea4bfd39520cbc729bb498f1d050fcb438
ggarber42 <ggarber@protonmail.com>
2022-08-11 01:13:15 -0300

    refactor:  use factory to enable stateful dialog

--- lib/widgets/form_widgets/form_widget_factory.dart
+++ lib/widgets/form_widgets/form_widget_factory.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
 
-import '../dialog_widgets/form_dialog_interface.dart';
-import '../dialog_widgets/text_widget_dialog.dart';
-import '../form_widgets/form_widget_text.dart';
-import 'form_widget_interface.dart';
-import 'form_widget_radio.dart';
+import '../dialog_widgets/widget_dialog_factory.dart';
 
 class FormWidgetFactory {
 
+  _showCreateDialog(BuildContext context, String selectedValue){
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return WidgetDialogFactory(selectedValue).makeWidget();
+        });
+  }
+
+
   Future<dynamic> createFormField(BuildContext context, String selectedValue) async {
-    FormWidget? newField;
-    switch (selectedValue) {
-      case 'text':
-        var name = await TextWidgetFormDialog().showCreateDialog(context);
-        newField = new FormWidgetText(name);
-        break;
-      default:
-        break;
-    }
+    var newField = await _showCreateDialog(context, selectedValue);
     return newField;
   }
 }
