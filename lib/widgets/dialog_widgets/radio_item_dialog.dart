37af9159f3e3fcfc0be26dd89c09a4747f457fb1
ggarber42 <ggarber@protonmail.com>
2022-08-15 23:22:48 -0300

    feat: radio item dialog

---
+++ lib/widgets/dialog_widgets/radio_item_dialog.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class RadioItemDialog extends StatelessWidget {
+  final TextEditingController _textEditingController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Nome do campo'),
+      content: Form(
+        key: _formKey,
+        child: TextFormField(
+          controller: _textEditingController,
+          decoration: InputDecoration(labelText: 'Digite aqui'),
+          textInputAction: TextInputAction.done,
+          validator: (String? value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter some text';
+            }
+            return null;
+          },
+        ),
+      ),
+      actions: [
+        FlatButton(
+          onPressed: () => Navigator.pop(context, false),
+          child: Text('Cancel'),
+        ),
+        FlatButton(
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              return Navigator.pop(context, _textEditingController.value.text);
+            }
+            return;
+          },
+          child: Text('Ok'),
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/dialog_widgets/radio_widget_dialog.dart
+++ lib/widgets/dialog_widgets/radio_widget_dialog.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'radio_item_dialog.dart';
+
 class RadioWidgetFormDialog extends StatefulWidget {
   @override
   _RadioWidgetFormDialogState createState() => _RadioWidgetFormDialogState();
@@ -8,87 +10,98 @@ class RadioWidgetFormDialog extends StatefulWidget {
 class _RadioWidgetFormDialogState extends State<RadioWidgetFormDialog> {
   final TextEditingController _textEditingController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  var _selectedValue;
   var optionQuantity = 1;
-  var _options = [
-    'kkkk',
-    'kkkk',
-    'kkkk',
-    'kkkk',
-    'kkkk',
-    'kkkk',
-    'kkkk',
-    'kkkk'
-  ];
+  var _options = [];
+
+  Future _showAddFieldDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return RadioItemDialog();
+        });
+  }
+
+  _handleDialog() async {
+    _selectedValue = await _showAddFieldDialog(context);
+    if (_selectedValue == null) return;
+    setState(() {
+      _options.add(_selectedValue);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Nome do campo'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            height: 100,
-            child: Form(
-              key: _formKey,
-              child: TextFormField(
-                controller: _textEditingController,
-                decoration: InputDecoration(labelText: 'Digite aqui'),
-                textInputAction: TextInputAction.done,
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-            ),
-          ),
-          Container(
-            height: 120,
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: Colors.grey),
-                top: BorderSide(color: Colors.grey),
-              ),
-            ),
-            child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: _options.length,
-                itemBuilder: (ctx, index) => Container(
-                      margin: EdgeInsets.symmetric(vertical: 5),
-                      child: Text(_options[index]),
-                    )),
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: Wrap(
-              alignment: WrapAlignment.center,
-              children: [
-                FlatButton(
-                  onPressed: () => Navigator.pop(context, false),
-                  child: Text('Cancel'),
-                ),
-                FlatButton(
-                  onPressed: () {
-                    print('kkk');
-                  },
-                  child: Text('Adicionar campo'),
-                ),
-                FlatButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      return Navigator.pop(
-                          context, _textEditingController.value.text);
+      content: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              height: 75,
+              child: Form(
+                key: _formKey,
+                child: TextFormField(
+                  controller: _textEditingController,
+                  decoration: InputDecoration(labelText: 'Digite aqui'),
+                  textInputAction: TextInputAction.done,
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Esse campo não pode ser';
                     }
-                    return;
+                    return null;
                   },
-                  child: Text('Finalizar'),
                 ),
-              ],
+              ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: double.infinity,
+              height: 100,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: _options.length,
+                  itemBuilder: (ctx, index) => Container(
+                        margin: EdgeInsets.symmetric(vertical: 5),
+                        child: Text(
+                          'Item $index: ${_options[index]}',
+                          textAlign: TextAlign.center,
+                        ),
+                      )),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  FlatButton(
+                    onPressed: () => Navigator.pop(context, false),
+                    child: Text('Cancel'),
+                  ),
+                  FlatButton(
+                    onPressed: () => _handleDialog(),
+                    child: Text('Adicionar campo'),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        return Navigator.pop(
+                            context, _textEditingController.value.text);
+                      }
+                      return;
+                    },
+                    child: Text('Finalizar'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
