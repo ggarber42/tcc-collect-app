873d66044f0b1cb864740d706617f85d9db9618b
ggarber42 <ggarber@protonmail.com>
2022-08-21 19:21:08 -0300

    fix: typo

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const DropMenuOptions = const [
   DropdownMenuItem(child: Text('Texto'), value: 'text'),
-  DropdownMenuItem(child: Text('Lista de Escolhas'), value: 'radio'),
+  DropdownMenuItem(child: Text('Lista de Opções'), value: 'radio'),
   DropdownMenuItem(child: Text('Foto'), value: 'img'),
   DropdownMenuItem(child: Text('GPS'), value: 'gps'),
 ];
