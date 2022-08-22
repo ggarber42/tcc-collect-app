16c90800e8c96695db8a6dc99362f778d5c0704e
guilherme_garber <guilherme.garber@dcg.com.br>
2022-08-21 21:49:29 -0300

    feat: input validation

--- lib/screens/add_form_fields_screen.dart
+++ lib/screens/add_form_fields_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../widgets/base_widgets/main_bar.dart';
 import '../widgets/base_widgets/main_drawer.dart';
+import '../widgets/dialog_widgets/alert_widget_dialog.dart';
 import '../widgets/dialog_widgets/new_field_dialog.dart';
 import '../widgets/dummy_widgets/dummy_field_factory.dart';
 
@@ -13,7 +14,9 @@ class AddFormFieldsScreen extends StatefulWidget {
 }
 
 class _AddFormFieldsScreenState extends State<AddFormFieldsScreen> {
-  var widgetList = [];
+  final TextEditingController _textEditingController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  var fieldList = [];
 
   Future _showFieldDialog(BuildContext context) {
     return showDialog(
@@ -24,14 +27,32 @@ class _AddFormFieldsScreenState extends State<AddFormFieldsScreen> {
         });
   }
 
+  _showWarningDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertWidgetFormDialog('Adicione pelo menos um campo!');
+        });
+  }
+
   _handleFieldDialog(BuildContext context) async {
     var selectedValue = await _showFieldDialog(context);
     if (selectedValue == null) return;
     var newFormField =
         await DummyFactoryField().createFormField(context, selectedValue);
     setState(() {
-      widgetList.add(newFormField.getWidgetBody());
+      fieldList.add(newFormField.getWidgetBody());
     });
+  }
+
+  _handleSubmit() {
+    var hasFieldAdded = fieldList.length > 0;
+    if (!hasFieldAdded) {
+      _showWarningDialog(context);
+    }
+    if (_formKey.currentState!.validate() && hasFieldAdded) {
+      print('KkkKKkk');
+    }
   }
 
   @override
@@ -46,24 +67,30 @@ class _AddFormFieldsScreenState extends State<AddFormFieldsScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Nome do Modelo',
-                ),
-              ),
-            ),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Form(
+                  key: _formKey,
+                  child: TextFormField(
+                    controller: _textEditingController,
+                    decoration: InputDecoration(labelText: 'Nome do modelo *'),
+                    textInputAction: TextInputAction.done,
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Esse campo não pode ser nulo';
+                      }
+                      return null;
+                    },
+                  ),
+                )),
             SingleChildScrollView(
               child: Container(
                 height: 400,
                 width: double.infinity,
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
                 child: ListView.builder(
-                    itemCount: widgetList.length,
+                    itemCount: fieldList.length,
                     itemBuilder: (ctx, index) {
-                      return widgetList[index];
+                      return fieldList[index];
                     }),
               ),
             )
@@ -83,10 +110,10 @@ class _AddFormFieldsScreenState extends State<AddFormFieldsScreen> {
         child: RaisedButton(
           color: Theme.of(context).colorScheme.primary,
           textColor: Colors.white,
-          elevation :0,
+          elevation: 0,
           materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          child: Text('Salvar Modelo' , style:  TextStyle(fontSize: 20)),
-          onPressed: () {},
+          child: Text('Salvar Modelo', style: TextStyle(fontSize: 20)),
+          onPressed: _handleSubmit,
         ),
       ),
     );
