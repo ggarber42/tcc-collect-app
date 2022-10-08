7bb09131c02f08028fdf362a6d593d3a708d5691
guilherme_garber <guilherme.garber@dcg.com.br>
2022-10-08 16:39:43 -0300

    refactor: split into widget

---
+++ lib/widgets/custom_widgets/input_tile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../screens/entries/entry_inputs.dart';
+
+class InputTile extends StatelessWidget {
+  final inputFields;
+  final Function addValue;
+
+  InputTile(this.inputFields, this.addValue);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GestureDetector(
+        child: Card(
+            child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.file_copy),
+              title: Text('Inputs'),
+            ),
+          ],
+        )),
+        onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => EntryInputsScreen(inputFields, addValue),
+            )),
+      ),
+    );
+  }
+}
