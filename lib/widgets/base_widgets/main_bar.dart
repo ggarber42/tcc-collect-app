88ba1123a1de1cc98819ebb1bc9056ff8235992b
guilherme_garber <guilherme.garber@dcg.com.br>
2022-10-08 15:17:23 -0300

    feat: add back button to appbar

--- lib/widgets/base_widgets/main_bar.dart
+++ lib/widgets/base_widgets/main_bar.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class MainBar extends StatelessWidget with PreferredSizeWidget {
-  String windowTitle;
+  final String windowTitle;
+  final bool hasBackButton;
 
-  MainBar({this.windowTitle = 'Collect-app'});
+  MainBar({this.windowTitle = 'Collect-app', this.hasBackButton = false});
   @override
   Widget build(BuildContext context) {
+    if (hasBackButton) {
+      return AppBar(
+        title: Text(windowTitle),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(false),
+        ),
+      );
+    }
     return AppBar(
       title: Text(windowTitle),
     );
