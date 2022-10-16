a378f1c6e5826436c01ca19d64cbe24c83a10203
guilherme_garber <guilherme.garber@dcg.com.br>
2022-10-16 10:19:15 -0300

    feat: field text

--- lib/widgets/field_widgets/field_text.dart
+++ lib/widgets/field_widgets/field_text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/extensions.dart';
 import '../../interfaces/field_interface.dart';
 
 class FieldText extends StatelessWidget implements Field {
@@ -21,19 +22,42 @@ class FieldText extends StatelessWidget implements Field {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-        controller: controller,
-        decoration: InputDecoration(
-            labelText: name,
-            icon: Icon(
-              Icons.text_fields,
-            )),
-        textInputAction: TextInputAction.done,
-        validator: (String? text) {
-          if (text == null || text.isEmpty) {
-            return 'Esse campo não pode ser nulo';
-          }
-          return null;
-        });
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: double.infinity,
+                child: Center(
+                    child: Text(name.capitalize(),
+                        style: TextStyle(fontSize: 17))),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.text_fields),
+                      fillColor: Colors.grey.shade200,
+                      filled: true,
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: controller,
+                    textInputAction: TextInputAction.done,
+                    validator: (String? text) {
+                      if (text == null || text.isEmpty) {
+                        return 'Esse campo não pode ser nulo';
+                      }
+                      return null;
+                    }),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
