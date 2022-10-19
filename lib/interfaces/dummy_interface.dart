f85faf546e60d1fd805d609a110ee6c260d9d4a1
guilherme_garber <guilherme.garber@dcg.com.br>
2022-10-18 23:08:38 -0300

    refactor: set index argument

--- lib/interfaces/dummy_interface.dart
+++ lib/interfaces/dummy_interface.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 abstract class Dummy {
   var dialog;
   get getType;
-  Widget getWidgetBody();
+  Widget getWidgetBody(index, function, context);
   init(BuildContext context);
 }
