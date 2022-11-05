49e9461cb213b690f996effe1402b71f79f277c4
guilherme_garber <guilherme.garber@dcg.com.br>
2022-11-05 12:13:00 -0300

    feat: view model form from firebase

--- lib/screens/model_form/list_online_models.dart
+++ lib/screens/model_form/list_online_models.dart
@@ -1,16 +1,9 @@
+import 'package:collect_app/screens/model_form/view_model.dart';
 import 'package:collect_app/widgets/custom_widgets/main_bar.dart';
 import 'package:flutter/material.dart';
 
-import '../../dao/form_model_dao.dart';
-import '../../dao/form_widget_dao.dart';
-import '../../dao/radio_option_dao.dart';
 import '../../facades/firestore.dart';
-import '../../models/form_model.dart';
-import '../../models/form_widget.dart';
-import '../../models/radio_option.dart';
-import '../../utils/helper.dart';
 import '../../widgets/custom_widgets/field_card.dart';
-import '../../widgets/dialog_widgets/confirmation_dialog.dart';
 
 class ListOnlineModelsScreen extends StatefulWidget {
   @override
@@ -19,9 +12,6 @@ class ListOnlineModelsScreen extends StatefulWidget {
 
 class _ListOnlineModelsScreenState extends State<ListOnlineModelsScreen> {
   final fireFacade = FirestoreFacade();
-  final modelFormDao = FormModelDAO();
-  final widgetDao = FormWidgetDAO();
-  final radioDao = RadioOptionDAO();
 
   Widget titleTile(obj) {
     return FieldCard(children: [
@@ -29,44 +19,13 @@ class _ListOnlineModelsScreenState extends State<ListOnlineModelsScreen> {
         leading: Icon(Icons.note_alt_rounded),
         title: Text(obj['name']),
         trailing: Icon(Icons.more_vert),
-        onTap: () async {
-          final res = await showDialog(
-              context: context,
-              barrierDismissible: true,
-              builder: (BuildContext context) {
-                return ConfirmationDialog(
-                  'Deseja criar esse modelo?',
-                );
-              });
-          print(res);
-          if (res == null) {
-            return;
-          }
-          if (!res) {
-            return;
-          }
-          print(obj);
-          final modelForm = FormModel(
-            name: obj['name'],
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => ViewModelScreen(obj),
+            ),
           );
-          final modelId = await modelFormDao.add(modelForm);
-          for (var field in obj['fields']) {
-            final widgetId = await widgetDao.add(
-              FormWidget.withModelId(
-                field['name'],
-                field['type'],
-                modelId,
-              ),
-            );
-            if (field['type'] == 'radio') {
-              for (var option in field['options']) {
-                await radioDao
-                    .add(RadioOption.withForeingKey(option, widgetId));
-              }
-            }
-          }
-          await Helper.showSnack(context, 'Modelo criado');
-          Navigator.popUntil(context, (Route<dynamic> route) => route.isFirst);
         },
       ),
     ]);
