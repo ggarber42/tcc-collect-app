d779bca66e3b0fdcb43134b378d1430fbc347fd1
guilherme_garber <guilherme.garber@dcg.com.br>
2022-11-05 12:13:56 -0300

    feat: email validation

--- lib/utils/helper.dart
+++ lib/utils/helper.dart
@@ -86,4 +86,13 @@ class Helper {
   static roundNumber(number) {
     return number.toStringAsFixed(3);
   }
+
+  static bool validateEmail(String value) {
+    String pattern =
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+        r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+        r"{0,253}[a-zA-Z0-9])?)*$";
+    RegExp regex = RegExp(pattern);
+    return regex.hasMatch(value);
+  }
 }
